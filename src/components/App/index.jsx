a823970a7e90c79c84b1d7bcc35dd5b1e1c83863
Johnny Rocket <0.snilcy@gmail.com>
2020-12-01 11:24:20 +0300

    18. Личный проект: оптимизируй это (часть 2)

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import * as pt from 'types'
 import { PageMain } from '../PageMain'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { PageLogin } from 'components/PageLogin'
 import { PageFavorite } from 'components/PageFavorite'
 import { PageOffer } from 'components/PageOffer'
+import { PageMainEmpty } from 'components/PageMainEmpty'
 
 export const App = ({ hotels }) => {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path='/'>
-					<PageMain hotels={hotels} />
+					{hotels.length ? <PageMain hotels={hotels} /> : <PageMainEmpty />}
 				</Route>
+
 				<Route exact path='/login' component={PageLogin} />
+
 				<Route exact path='/favorites'>
 					<PageFavorite hotels={hotels} />
 				</Route>
+
 				<Route exact path='/offer/:id'>
 					{({ match }) => {
 						const hotel = hotels.find(({ id }) => +match.params.id === id)
-						if (!hotel) return <Redirect to='/404' />
+						if (!hotel) return 404
 						return <PageOffer hotel={hotel} />
 					}}
 				</Route>
+
 				<Route path='*'>404</Route>
-				<Route path='/404'>404</Route>
 			</Switch>
 		</BrowserRouter>
 	)
